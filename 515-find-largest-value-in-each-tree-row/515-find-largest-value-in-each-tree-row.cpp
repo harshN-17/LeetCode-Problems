b4b3481cda418a078359675170d5d483876fb440
Harsh Narayan <96466588+harshN-17@users.noreply.github.com>
2022-06-23 11:13:03 +0530

    Time: 27 ms (19.04%), Space: 22.2 MB (68.06%) - LeetHub

---
+++ 515-find-largest-value-in-each-tree-row/515-find-largest-value-in-each-tree-row.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> largestValues(TreeNode* root) {
+        if(!root) return {};
+        queue<TreeNode*> q;
+        q.push(root);
+        vector<int> res;
+        while(!q.empty()) {
+            int size = q.size();
+            int mx = INT_MIN;
+            while(size--) {
+                TreeNode* cur = q.front();
+                q.pop();
+                if(cur->left) q.push(cur->left);
+                if(cur->right) q.push(cur->right);
+                mx = max(mx, cur->val);
+            }
+            res.push_back(mx);
+        }
+        return res;
+    }
+};
