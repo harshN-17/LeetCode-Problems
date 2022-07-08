261fce4722b18290bec636db306a3723e299fd09
Harsh Narayan <96466588+harshN-17@users.noreply.github.com>
2022-07-08 15:57:59 +0530

    Time: 89 ms (5.05%), Space: 27 MB (5.12%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     unordered_map<string, int> dp;
-    int solve(vector<int>& stones, int index, int sum1, int sum2) {
-        if(index == stones.size()) return abs(sum1 - sum2);
-        string key = to_string(sum1) + "|" + to_string(sum2);
+    int solve(vector<int>& stones, int index, int sum) {
+        if(index == stones.size()) return sum;
+        string key = to_string(sum) + "|" + to_string(index);
         if(dp.find(key) != dp.end()) return dp[key];
-        return dp[key] = min(solve(stones, index + 1, sum1 + stones[index], sum2), solve(stones, index + 1, sum1, sum2 + stones[index])) ;
+        return dp[key] = min(solve(stones, index + 1, sum + stones[index]), solve(stones, index + 1, abs(sum - stones[index]))) ;
     }
     int lastStoneWeightII(vector<int>& stones) {
-        return solve(stones, 0, 0, 0);
+        return solve(stones, 0, 0);
     }
 };
