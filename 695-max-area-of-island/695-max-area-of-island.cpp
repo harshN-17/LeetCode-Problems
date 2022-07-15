c9c81183e8a1770fa284855502074474d64e73c4
Harsh Narayan <96466588+harshN-17@users.noreply.github.com>
2022-07-15 14:15:13 +0530

    Time: 15 ms (97.12%), Space: 23.2 MB (89.29%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    int solve(vector<vector<int>>& grid, vector<vector<bool>> &vis, int i, int j){
+    int solve(vector<vector<int>>& grid, int i, int j){
         if(i<0 or j<0 or i>=grid.size() or j>=grid[0].size() or grid[i][j]==0)
             return 0;
-        if(vis[i][j]) return 0;
-        vis[i][j] = true;
-        int left = solve(grid, vis, i,j-1);
-        int right = solve(grid, vis, i,j+1);
-        int up = solve(grid, vis, i-1,j);
-        int down = solve(grid, vis, i+1,j);
+        grid[i][j] = 0;
+        int left = solve(grid, i,j-1);
+        int right = solve(grid, i,j+1);
+        int up = solve(grid, i-1,j);
+        int down = solve(grid, i+1,j);
+        // grid[i][j] = 1;
         // cout<<left<<' '<<right<<' '<<up<<' '<<down<<endl;
         return 1 + left + right + up + down;
     }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         int ans=0;
-        vector<vector<bool>> vis(grid.size(), vector<bool>(grid[0].size(),false));
         for(int i=0; i<grid.size(); i++){
             for(int j=0; j<grid[0].size(); j++){
-                if(!vis[i][j] and grid[i][j]){
-                    ans = max(ans,solve(grid, vis, i, j));
+                if(grid[i][j]){
+                    ans = max(ans,solve(grid, i, j));
                 }
             }
         }
