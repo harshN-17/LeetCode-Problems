b12662f4bdabd3cd20306137b8ab5950df547c56
Harsh Narayan <96466588+harshN-17@users.noreply.github.com>
2022-08-02 12:39:01 +0530

    Time: 229 ms (37.44%), Space: 43.6 MB (38.50%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -6,25 +6,53 @@ public:
             graph[it[0]].push_back({it[1], it[2]});
         }
         vector<int> dist(n + 1, INT_MAX);
-        queue<int> q;
-        q.push(k);
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int,int>>> pq;
+        pq.push({0,k});
+        dist[0] = 0;
         dist[k] = 0;
-        while(!q.empty()) {
-            int t = 0;  
-            int cur = q.front();
-            q.pop();
-            for(auto it: graph[cur]) {
-                if(dist[it.first] > dist[cur] + it.second) {
-                    dist[it.first] = dist[cur] + it.second;
-                    q.push(it.first);
+        while(!pq.empty()) {
+            auto cur = pq.top();
+            pq.pop();
+            for(auto it: graph[cur.second]) {
+                if(dist[it.first] > dist[cur.second] + it.second) {
+                    dist[it.first] = dist[cur.second] + it.second;
+                    pq.push({dist[it.first], it.first});
                 }
             }
         }
-        int res = 0;
-        for(int i = 1; i <= n; i++) {
-            if(dist[i] == INT_MAX) return -1;
-            res = max(res, dist[i]);
-        }
-        return res;
+        int res = *max_element(dist.begin(), dist.end());
+        return res == INT_MAX ? -1 : res;
     }
 };
+
+
+
+
+
+
+
+        // vector<vector<pair<int, int>>> graph(n + 1);
+        // for(auto it: times) {
+        //     graph[it[0]].push_back({it[1], it[2]});
+        // }
+        // vector<int> dist(n + 1, INT_MAX);
+        // queue<int> q;
+        // q.push(k);
+        // dist[k] = 0;
+        // while(!q.empty()) {
+        //     int t = 0;  
+        //     int cur = q.front();
+        //     q.pop();
+        //     for(auto it: graph[cur]) {
+        //         if(dist[it.first] > dist[cur] + it.second) {
+        //             dist[it.first] = dist[cur] + it.second;
+        //             q.push(it.first);
+        //         }
+        //     }
+        // }
+        // int res = 0;
+        // for(int i = 1; i <= n; i++) {
+        //     if(dist[i] == INT_MAX) return -1;
+        //     res = max(res, dist[i]);
+        // }
+        // return res;
