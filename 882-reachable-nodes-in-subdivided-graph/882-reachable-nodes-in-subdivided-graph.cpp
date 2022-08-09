2db6258809f1a2ca449c74d12492531a4b0d8c4a
Harsh Narayan <96466588+harshN-17@users.noreply.github.com>
2022-08-09 16:03:52 +0530

    Time: 192 ms (94.43%), Space: 51.5 MB (70.16%) - LeetHub

---
+++ 882-reachable-nodes-in-subdivided-graph/882-reachable-nodes-in-subdivided-graph.cpp
@@ -0,0 +1,36 @@
+# define pii pair<int, int>
+class Solution {
+public:
+    int reachableNodes(vector<vector<int>>& edges, int maxMoves, int n) {
+        vector<vector<pii>> graph(n);
+        for(auto edge: edges) {
+            graph[edge[0]].push_back({edge[1], edge[2]});            
+            graph[edge[1]].push_back({edge[0], edge[2]});
+        }
+        priority_queue<pii, vector<pii>, greater<pii>> pq;
+        vector<int> dist(n, INT_MAX);
+        dist[0] = 0;
+        pq.push({0, 0});
+        while(!pq.empty()) {
+            auto [curDist, curNode] = pq.top();
+            pq.pop();
+            if(dist[curNode] < curDist) continue;
+            for(auto child: graph[curNode]) {
+                if(dist[child.first] > curDist + child.second + 1) {
+                    dist[child.first] = curDist + child.second + 1;
+                    pq.push({dist[child.first], child.first});
+                }
+            }
+        }
+        int res = 0;
+        for(auto x: dist) {
+            if(x <= maxMoves) res++;
+        }
+        for(auto edge: edges) {
+            int x = max(0, maxMoves - dist[edge[0]]);
+            int y = max(0, maxMoves - dist[edge[1]]);
+            res += min(edge[2], x + y);
+        }
+        return res;
+    }
+};
