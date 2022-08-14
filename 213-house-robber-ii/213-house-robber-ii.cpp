bb19df2173a8ccd9e9ad52a7d71e09433b05d39d
Harsh Narayan <96466588+harshN-17@users.noreply.github.com>
2022-08-14 20:04:51 +0530

    Time: 3 ms (58.76%), Space: 7.9 MB (63.40%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int left, int right){
-        int premax=0, curmax=0;
-        for(int i=left; i<right; i++) {
-            int t = curmax;
-            curmax = max(premax + nums[i], curmax);
-            premax = t;
+    int solve(vector<int>& nums, int left, int right) {
+        int dp1 = 0, dp2 = 0;
+        for(int i = left; i <= right; i++) {
+            int temp = max(dp1 + nums[i], dp2);
+            dp1 = dp2;
+            dp2 = temp;
         }
-        return curmax;
+        return dp2;
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if(n==1) return nums[0];
-        if(n==2) return max(nums[0],nums[1]);
-        
-        return max(solve(nums, 0, n -1) , solve(nums, 1, n));
+        if(n == 1) return nums[0];
+        if(n == 2) return max(nums[0], nums[1]);
+        return max(solve(nums, 0, nums.size() - 2), solve(nums, 1, nums.size() - 1));
     }
 };
