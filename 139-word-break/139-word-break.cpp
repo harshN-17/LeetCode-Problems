aaf5d16e12b93e0124d36779296aaa65fc50da9b
Harsh Narayan <96466588+harshN-17@users.noreply.github.com>
2022-08-21 22:26:43 +0530

    Time: 8 ms (78.23%), Space: 8.2 MB (76.81%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
-    int dp[301];
-    bool solve(string s, vector<string>& wordDict, int index){
-        if(s.size()<=index){
-            return true;
-        }
-        if(dp[index] != -1) return dp[index];
-        for(int i=index+1; i<=s.size(); i++){
-            string left = s.substr(index,i-index);
-            // cout<<i<<' '<<left<<' '<<s<<' '<<endl;
-            if(find(wordDict.begin(), wordDict.end(), left) != wordDict.end()){
-                    if(solve(s, wordDict, i)){
-                        dp[index] = 1;
-                        return true;
-                    }
-                }
+    vector<int> memo;
+    bool solve(string &s, unordered_set<string>& dict, int idx) {
+        if(idx == s.size()) return true;
+        if(memo[idx] != -1) return memo[idx];
+        string temp = "";
+        for(int i = idx; i < s.size(); i++) {
+            temp += s[i];
+            if(dict.find(temp) != dict.end()) {
+                if(solve(s, dict, i + 1)) return memo[idx] = true;
             }
-        dp[index] = 0;
-        return false;
+        }
+        return memo[idx] = false;
     }
     bool wordBreak(string s, vector<string>& wordDict) {
-        memset(dp, -1, sizeof(dp));
-        return solve(s,wordDict,0);
+        int n = s.size();
+        memo.assign(n + 1, -1);
+        unordered_set<string> dict(wordDict.begin(), wordDict.end());
+        return solve(s, dict, 0);
     }
 };
